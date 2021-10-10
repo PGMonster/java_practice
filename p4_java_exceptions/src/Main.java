1b8eb97f9791343bc681a71c428e8691ac817606
PGMonster <15026582173@163.com>
2021-10-11 00:29:56 +0800

    update projectes structure

---
+++ p4_java_exceptions/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Java 异常处理
+ *  1. java 异常
+ *      -1. Java使用异常来表示错误，并通过try ... catch捕获异常；
+ *
+ *      -2. Java的异常是class，并且从Throwable继承；
+ *
+ *      -3. Error是无需捕获的严重错误，Exception是应该捕获的可处理的错误；
+ *
+ *      -4. RuntimeException无需强制捕获，非RuntimeException（Checked Exception）需强制捕获，或者用throws声明；
+ */
+
+public class Main {
+}
